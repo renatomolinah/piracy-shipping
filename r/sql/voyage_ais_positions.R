938fc1e65314a5c1f53b4433bf3ed49eab82d90e
Gavin McDonald <gmcdonald@gavins-imac.esm.ucsb.edu>
2018-05-08 12:07:40 -0700

    add fuel consumption to ais table

--- r/sql/voyage_ais_positions.R
+++ r/sql/voyage_ais_positions.R
@@ -3,8 +3,31 @@
 sql <- 
   "
 SELECT
+mmsi,
+vessel_type,
+fishing,
+through_hotspot,
+start_lat,
+start_lon,
+start_timestamp,
+end_lat,
+end_lon,
+end_timestamp,
+hours,
+ais_speed,
+avg_distance_km,
+from_anchorage,
+from_anchorage_id,
+departure_timestamp,
+to_anchorage,
+to_anchorage_id,
+arrival_timestamp,
+fishing,
+(hours*main_load_factor*main_sfc_low*engine_power/1000000 + hours*aux_load_factor*aux_sfc*aux_engine_power/1000000) total_fuel_consumption__low_bound,
+(hours*main_load_factor*main_sfc*engine_power/1000000 + hours*aux_load_factor*aux_sfc*aux_engine_power/1000000) total_fuel_consumption__high_bound,
+FROM(
+SELECT
 ais_info.mmsi mmsi,
-ais_info.vessel_type vessel_type,
 ais_info.fishing fishing,
 (CASE WHEN cluster_info.cluster_filter 
 THEN 1
@@ -29,10 +52,23 @@ ais_info.arrival_timestamp arrival_timestamp,
 THEN 1
 ELSE 0
 END) fishing,
+ais_info.vessel_type vessel_type,
+ais_info.length length, 
+ais_info.engine_power engine_power, 
+ais_info.crew crew, 
+ais_info.tonnage tonnage, 
+ais_info.aux_engine_power aux_engine_power, 
+ais_info.design_speed_ihs design_speed_ihs, 
+ais_info.main_sfc main_sfc, 
+ais_info.main_sfc_low main_sfc_low, 
+ais_info.aux_sfc aux_sfc,
+(CASE WHEN ais_info.ais_speed > ais_info.design_speed_ihs THEN 0.9*(1 + 0.285)/1.285
+ELSE 0.9*(POW(ais_info.ais_speed/ais_info.design_speed_ihs, 3) + 0.285)/1.285
+END) main_load_factor,
+0.3 aux_load_factor,
 FROM(
 SELECT
 a.mmsi mmsi,
-vessel_type,
 fishing,
 start_lat,
 start_lon,
@@ -48,7 +84,17 @@ from_anchorage_id,
 departure_timestamp,
 to_anchorage,
 to_anchorage_id,
-arrival_timestamp
+arrival_timestamp,
+vessel_type,
+length, 
+engine_power, 
+crew, 
+tonnage, 
+aux_engine_power, 
+design_speed_ihs, 
+main_sfc, 
+main_sfc_low, 
+aux_sfc
 FROM (
 SELECT
 mmsi,
@@ -73,13 +119,22 @@ JOIN (
 SELECT
 mmsi,
 YEAR(departure_timestamp) year,
-vessel_type,
 from_anchorage_name from_anchorage,
 from_anchorage_id,
 departure_timestamp,
 to_anchorage_name to_anchorage,
 to_anchorage_id,
-arrival_timestamp
+arrival_timestamp,
+vessel_type,
+length, 
+engine_power, 
+crew, 
+tonnage, 
+aux_engine_power, 
+design_speed_ihs, 
+main_sfc, 
+main_sfc_low, 
+aux_sfc
 FROM
 `ucsb-gfw.piracy.voyages_with_anchorages`) anchorages
 ON
@@ -93,7 +148,7 @@ cluster_filter
 FROM
 [piracy.cluster_filters]) cluster_info
 ON
-ais_info.year = cluster_info.year"
+ais_info.year = cluster_info.year)"
 
 # source for bunker fuel data? http://www.bunkerindex.com/prices/indices.php
 # probably want IFO380, IFO180, or composite index
