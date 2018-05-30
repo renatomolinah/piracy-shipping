f627008bb2849643a1fa144ba61ea55951a5116e
Gavin McDonald <gmcdonald@lempo.esm.ucsb.edu>
2018-05-30 15:52:36 -0700

    updating figs for renato

--- r/sql/gridded_shipping_hours.R
+++ r/sql/gridded_shipping_hours.R
@@ -2,24 +2,27 @@
 # Data for shipping map
 sql <-
   "
+#standardSQL
 SELECT
-YEAR(start_timestamp) year,
+EXTRACT(year FROM departure_timestamp) year,
 FLOOR(start_lat / 0.5) * 0.5 lat_bin,
 FLOOR(start_lon / 0.5) * 0.5 lon_bin,
 SUM(hours) hours
 FROM
-[ucsb-gfw:piracy.voyage_ais_positions]
+`piracy.voyage_ais_positions`
 GROUP BY
 year,
 lat_bin,
-lon_bin
-WHERE
-vessel_type IN (cargo, tanker, reefer)"
+lon_bin"
 
 bq_table(project = project,table = "gridded_shipping_hours",dataset = "piracy") %>% 
   bq_table_delete()
-bq_table(project = project,table = "gridded_shipping_hours",dataset = "piracy") %>% 
-  bq_table_upload(values = gridded_shipping_hours)
+
+bq_project_query(project,query = sql, destination_table = bq_table(project = project,table = "gridded_shipping_hours",dataset = "piracy"),
+                 allowLargeResults = TRUE)
+
+gridded_shipping_hours <- bq_project_query(project, "SELECT * FROM `piracy.gridded_shipping_hours`") %>%
+  bq_table_download(max_results = Inf)
 
 # Arrange and cache data for later
 # Make smaller, to 1 degree cells
@@ -29,6 +32,7 @@ gridded_shipping_hours_processed <- gridded_shipping_hours %>%
            lon_bin < 180 &
            lon_bin > - 180) %>%
   group_by(year,lat_bin,lon_bin) %>%
-  summarize(hours = sum(hours,na.rm=TRUE))
+  summarize(hours = sum(hours,na.rm=TRUE)) %>%
+  filter(!is.na(year))
 
-write_csv(gridded_shipping_hours_processed,"../processed_data/gridded_shipping_hours_processed.csv")
+write_csv(gridded_shipping_hours_processed,"processed_data/gridded_shipping_hours_processed.csv")
