f44ce06a55ea3fcf503d632c815e7fded6608453
Gavin McDonald <gmcdonald@Gavins-iMac.local>
2019-03-01 14:35:30 -0800

    fixing point analysis queries. looking good!

--- r/point_analysis.R
+++ r/point_analysis.R
@@ -95,6 +95,7 @@ sql<-"#standardSQL
   WITH attack_info_1 AS(
   SELECT
     reference attack_reference_1,
+    attack_eez,
     date date_attack_1,
     ST_GEOGFROMTEXT(point) attack_geog_1
   FROM
@@ -117,6 +118,8 @@ sql<-"#standardSQL
   SELECT
     attack_reference_1 attack_reference,
     attack_reference_2,
+    attack_eez,
+    date_attack_1 date_attack,
     ST_Distance(attack_geog_1,
       attack_geog_2)/1000 distance_between_attacks_km,
     DATE_DIFF(date_attack_1,
@@ -183,12 +186,13 @@ bq_table(project = project,table = "point_analysis_full",dataset = "piracy") %>%
 bq_project_query(project,query = sql, destination_table =  bq_table(project = project,table = "point_analysis_full",dataset = "piracy"),
                  use_legacy_sql = FALSE, allowLargeResults = TRUE)
 #https://stackoverflow.com/questions/52485871/distinct-count-across-bigquery-arrays
+
 sql<-"#standardSQL
   CREATE TEMP FUNCTION DistinctCOUNT(arr ANY TYPE) AS ( (
     SELECT
       COUNT(DISTINCT x)
     FROM
-      UNNEST(arr) AS x) ); WITH shipping_info AS(
+      UNNEST(arr) AS x) );
   SELECT
     attack_reference,
     attack_eez,
@@ -210,10 +214,18 @@ sql<-"#standardSQL
     attack_eez,
     date_attack,
     days_since_attack_bin,
-    distance_to_attack_km_bin),
-  attack_info AS(
-  SELECT
+    distance_to_attack_km_bin"
+
+bq_table(project = project,table = "point_summary_shipping",dataset = "piracy") %>% 
+  bq_table_delete()
+bq_project_query(project,query = sql, destination_table =  bq_table(project = project,table = "point_summary_shipping",dataset = "piracy"),
+                 use_legacy_sql = FALSE, allowLargeResults = TRUE)
+
+sql<-"#standardSQL
+SELECT
     attack_reference,
+    attack_eez,
+    date_attack,
     COUNT(*) number_attacks,
     (CASE
         WHEN days_between_attacks = 0 THEN 30
@@ -226,16 +238,45 @@ sql<-"#standardSQL
     `ucsb-gfw.piracy.point_attacks_crossed`
   GROUP BY
     attack_reference,
+    attack_eez,
+    date_attack,
     days_since_attack_bin,
-    distance_to_attack_km_bin)
+    distance_to_attack_km_bin"
+
+bq_table(project = project,table = "point_summary_attacks",dataset = "piracy") %>% 
+  bq_table_delete()
+bq_project_query(project,query = sql, destination_table =  bq_table(project = project,table = "point_summary_attacks",dataset = "piracy"),
+                 use_legacy_sql = FALSE, allowLargeResults = TRUE)
+
+sql<-"#standardSQL
+WITH
+  shipping_info AS(
+  SELECT
+    *
+  FROM
+    `ucsb-gfw.piracy.point_summary_shipping`),
+  attack_info AS(
+  SELECT
+    *
+  FROM
+    `ucsb-gfw.piracy.point_summary_attacks`)
 SELECT
-  *
+  * EXCEPT (shipping_distance_traveled_km,
+    shipping_hours,
+    unique_number_voyages,
+    number_attacks),
+  IFNULL(shipping_distance_traveled_km, 0) shipping_distance_traveled_km,
+  IFNULL(shipping_hours, 0) shipping_hours,
+  IFNULL(unique_number_voyages, 0) unique_number_voyages,
+  IFNULL(number_attacks, 0) number_attacks
 FROM
-  shipping_info
-LEFT JOIN
+  shipping_info FULL OUTER
+JOIN
   attack_info USING(attack_reference,
     days_since_attack_bin,
-    distance_to_attack_km_bin)"
+    distance_to_attack_km_bin,
+    attack_eez,
+    date_attack)"
 
 bq_table(project = project,table = "point_analysis_summary",dataset = "piracy") %>% 
   bq_table_delete()
@@ -250,9 +291,12 @@ WHERE
 days_since_attack_bin > -390 
 AND days_since_attack_bin < 390 
 AND distance_to_attack_km_bin <= 500
+AND date_attack >= DATE(TIMESTAMP('2013-01-01'))
+AND date_attack < DATE(TIMESTAMP('2018-01-01'))
 "
 
 point_analysis_summary <- bq_project_query(project, sql) %>%
   bq_table_download(max_results = Inf)
+
 write_csv(point_analysis_summary,path="point_analysis_summary.csv")
 
