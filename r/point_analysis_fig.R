98426ec40de573fec320d91a57752f3b1e284c4f
Gavin McDonald <gmcdonald@Gavins-iMac.local>
2019-03-21 14:19:33 -0700

    new point analysis fig for renato

--- r/point_analysis_fig.R
+++ r/point_analysis_fig.R
@@ -31,39 +31,35 @@ test %>%
 baseline<-point_analysis_summary %>%
   filter(days_since_attack_bin<0) %>%
   group_by(attack_reference,distance_to_attack_km_bin) %>%
-  summarize(shipping_hours_baseline = median(shipping_hours,na.rm=TRUE),
-            shipping_distance_baseline = median(shipping_distance_traveled_km,na.rm=TRUE)) %>%
+  summarize(shipping_hours_baseline = mean(shipping_hours,na.rm=TRUE),
+            shipping_distance_baseline = mean(shipping_distance_traveled_km,na.rm=TRUE),
+            unique_number_voyages_baseline = mean(unique_number_voyages,na.rm=TRUE)) %>%
   ungroup()
 
 grouped <- point_analysis_summary %>%
   left_join(baseline,by=c("attack_reference","distance_to_attack_km_bin")) %>%
-  mutate(shipping_hours_normalized = ifelse(shipping_hours_baseline==0,0,shipping_hours/shipping_hours_baseline),
-         shipping_distance_normalized = ifelse(shipping_distance_baseline==0,0,shipping_distance_traveled_km/shipping_distance_baseline)) %>%
+  mutate(shipping_hours_normalized = ifelse(shipping_hours_baseline==0,NA,shipping_hours/shipping_hours_baseline),
+         shipping_distance_normalized = ifelse(shipping_distance_baseline==0,NA,shipping_distance_traveled_km/shipping_distance_baseline),
+         unique_number_voyages_normalized = ifelse(unique_number_voyages_baseline==0,NA,unique_number_voyages/unique_number_voyages_baseline)) %>%
   rename(`distance km` = distance_to_attack_km_bin) %>%
   mutate(timing = ifelse(days_since_attack_bin<0,"Before","After"))
 
-grouped  %>%
+plot <- grouped  %>%
+  filter(`distance km` == 100) %>%
   #filter(attack_reference==test_case) %>%
   group_by(timing,days_since_attack_bin,`distance km`) %>%
-  summarize(shipping_hours_normalized = median(shipping_hours_normalized,na.rm=TRUE),
-            shipping_distance_normalized = median(shipping_distance_normalized,na.rm=TRUE)) %>%
+  summarize(`Hours traveled` = mean(shipping_hours_normalized,na.rm=TRUE),
+            `Distance traveled [km]` = mean(shipping_distance_normalized,na.rm=TRUE),
+            `Unique number of voyages` = mean(unique_number_voyages_normalized,na.rm=TRUE)) %>%
   ungroup() %>%
-  ggplot(aes(x=days_since_attack_bin,y=shipping_hours_normalized)) +
+  gather(indicator,value,`Hours traveled`:`Unique number of voyages`,-days_since_attack_bin,-timing) %>%
+  ggplot(aes(x=days_since_attack_bin,y=value)) +
   geom_smooth(aes(group=timing)) +
-  facet_wrap(.~`distance km`,labeller = label_both) +
+  facet_wrap(.~indicator,scales="free",ncol=1) +
   geom_vline(xintercept = 0,linetype=2) +
   geom_hline(yintercept = 1,linetype=1) +
-  theme_bw() +
-  labs(title=paste0("Shipping hours before and after attacks\nfor different distances from attack [km]"),
-       subtitle = "Shipping hours normalized by mean of pre-attack hours\nMedian value across all vessels",
-       x="Days since attack",
-       y="Normalized shipping hours") 
-
-point_analysis_summary %>% 
-  group_by(days_since_attack_bin,distance_to_attack_km_bin) %>%
-  summarize(median = sum(shipping_hours,na.rm=TRUE)) %>%
-  ungroup() %>%
-  rename(`distance km` = distance_to_attack_km_bin) %>%
-  ggplot(aes(x=days_since_attack_bin,y=median)) +
-  geom_line()+
-  facet_wrap(.~`distance km`,labeller = label_both)
+  theme_minimal(base_size = 18) +
+  labs(x="Days since attack",
+       y="Mean normalized metric value",
+       title="Normalized metrics relative to pre-attack baseline,\n50-100km from attack, mean across all attacks") 
+ggsave(filename = "figs/point_analysis.eps", plot = plot, device = cairo_ps, dpi=300, width = 8.5, height = 8.5)
