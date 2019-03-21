3ac1331fadea866ab3264e105406cf8adf2d063f
Gavin McDonald <gmcdonald@Gavins-iMac.local>
2019-03-21 14:20:20 -0700

    new fig for renato

--- r/point_analysis_fig.R
+++ r/point_analysis_fig.R
@@ -1,33 +1,5 @@
 library(tidyverse)
 point_analysis_summary <- read_csv("point_analysis_summary.csv")
-
-test_case <- 700
-test_case <- unique(point_analysis_summary$attack_reference)[test_case]
-test<-point_analysis_summary %>% 
-  filter(attack_reference==test_case)
-
-test_baseline<-test %>%
-  filter(days_since_attack_bin<0) %>%
-  group_by(distance_to_attack_km_bin) %>%
-  summarize(shipping_hours_baseline = mean(shipping_hours)) %>%
-  ungroup()
-test <- test %>%
-  left_join(test_baseline,by="distance_to_attack_km_bin") %>%
-  mutate(shipping_hours_normalized = shipping_hours/shipping_hours_baseline) %>%
-  rename(`distance km` = distance_to_attack_km_bin)
-
-test %>%
-  ggplot(aes(x=days_since_attack_bin,y=shipping_hours_normalized)) +
-  geom_line() +
-  facet_wrap(.~`distance km`,labeller = label_both) +
-  geom_vline(xintercept = 0,linetype=2) +
-  geom_hline(yintercept = 1,linetype=1) +
-  theme_bw() +
-  labs(title=paste0("Shipping hours before and after attack id `",test_case,"`\nfor different distances from attack [km]"),
-       subtitle = "Shipping hours normalized by mean of pre-attack hours",
-       x="Days since attack",
-       y="Normalized shipping hours")
-
 baseline<-point_analysis_summary %>%
   filter(days_since_attack_bin<0) %>%
   group_by(attack_reference,distance_to_attack_km_bin) %>%
