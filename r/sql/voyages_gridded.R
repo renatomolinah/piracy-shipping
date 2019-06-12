0046fbc227d971171f8da44d0bd43c95fcdf2461
Gavin McDonald <gmcdonald@Gavins-iMac.local>
2019-06-12 08:49:54 -0700

    also calculate instantaneous fuel consumption

--- r/sql/voyages_gridded.R
+++ r/sql/voyages_gridded.R
@@ -6,59 +6,45 @@
 sql <-glue::glue("
   #standardSQL
   WITH
+  vessel_info AS(
+  SELECT
+  *
+  FROM 
+  `piracy.vessel_info`
+  ),
 ais_info AS(
   SELECT
   mmsi,
-  vessel_type,
-  flag,
-  length,
-  engine_power,
-  crew,
-  tonnage,
-  aux_engine_power,
-  design_speed,
-  main_sfc,
-  aux_sfc,
-  FLOOR(start_lat/5) * 5 lat_bin,
-  FLOOR(start_lon/5) * 5 lon_bin,
+  FLOOR(lat/5) * 5 lat_bin,
+  FLOOR(lon/5) * 5 lon_bin,
   from_anchorage_id,
-  from_anchorage_name,
-  from_port_name,
   departure_timestamp,
   DATE(departure_timestamp) departure_date,
-  to_anchorage_id,
-  to_anchorage_name,
-  to_port_name,
-  arrival_timestamp,
   SUM(hours) hours,
   SUM(avg_distance_km) distance_km,
   AVG(heading) heading,
+  SUM(hours*(0.8 * POW(implied_speed/design_speed, 3)))*main_sfc*engine_power/1000000 main_fuel_consumption_mt_inst,
+  SUM(hours)*0.5*aux_sfc*aux_engine_power/1000000 aux_fuel_consumption_mt_inst,
   {clusters_aggregated}
   FROM
   `piracy.voyage_ais_positions`
+  JOIN
+  SELECT
+  mmsi,year,design_speed,main_sfc,engine_power,aux_engine_power,aux_sfc
+  FROM vessel_info
+  USING(mmsi,year)
   GROUP BY
   mmsi,
-  vessel_type,
-  flag,
-  length,
-  engine_power,
-  crew,
-  tonnage,
-  aux_engine_power,
-  design_speed,
-  main_sfc,
-  aux_sfc,
   lat_bin,
-  lon_bin,
+  lon_bin
   from_anchorage_id,
-  from_anchorage_name,
-  from_port_name,
-  departure_timestamp,
-  departure_date,
-  to_anchorage_id,
-  to_anchorage_name,
-  to_port_name,
-  arrival_timestamp),
+  departure_timestamp),
+  voyage_info AS(
+  SELECT
+  *
+  FROM
+  `piracy.voyages_with_anchorages`
+  ),
 wind AS(
   SELECT
   *
@@ -74,7 +60,8 @@ master AS(
   ais_info.departure_date departure_date,
   ais_info.lat_bin lat_bin,
   ais_info.lon_bin lon_bin,
-  mmsi,
+  ais_info.mmsi,
+  voyage_info.year,
   vessel_type,
   flag,
   length,
@@ -86,15 +73,16 @@ master AS(
   main_sfc,
   aux_sfc,
   from_anchorage_id,
-  from_anchorage_name,
   from_port_name,
   departure_timestamp,
   to_anchorage_id,
-  to_anchorage_name,
   to_port_name,
   arrival_timestamp,
   distance_km,
   hours,
+  main_fuel_consumption_mt_inst,
+  aux_fuel_consumption_mt_inst,
+  (main_fuel_consumption_mt_inst + aux_fuel_consumption_mt_inst) total_fuel_consumption_mt_inst,
 {clusters_aggregated_2},
 days_since_attack,
 grid_has_previous_attacks,
@@ -121,9 +109,18 @@ heading
   ON
   ais_info.departure_date = piracy_attacks.date
   AND ais_info.lat_bin = piracy_attacks.lat_bin
-  AND ais_info.lon_bin = piracy_attacks.lon_bin)
+  AND ais_info.lon_bin = piracy_attacks.lon_bin
+  LEFT JOIN
+  vessel_info
+  USING(mmsi,year)
+  LEFT JOIN
+  voyage_info
+  USING(mmsi,departure_timestamp,from_anchorage_id))
 SELECT
-*
+*,
+# Wind vector relative to direction of travel
+# Positive is tailwind, negative is headwind
+COS(RADIANS(direction_degrees - heading)) * speed_m_s wind_vec
   FROM
 master
 ")
