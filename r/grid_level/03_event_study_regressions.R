ac05a616675fbae567531f5f8f2b83fd41fa5db8
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2023-11-28 13:25:48 -0800

    Updated code

--- r/grid_level/03_event_study_regressions.R
+++ r/grid_level/03_event_study_regressions.R
@@ -22,102 +22,68 @@ pacman::p_load(
   tidyverse
 )
 
-sf_use_s2(F)
 # Load data --------------------------------------------------------------------
 data <- readRDS(file = here("processed_data",
                              "attacks_and_activity_by_grid.rds"))
 
-grid <- data %>% 
-  select(grid_id, lat_bin, lon_bin) %>% 
-  distinct() %>% 
-  st_as_sf(coords = c("lon_bin", "lat_bin"), crs = 4326)
-
-fao_regions <- st_read(dsn = here("data", "fao_regions.gpkg"))
-
-grid_fao <- grid %>% 
-  st_join(fao_regions, join = st_nearest_feature) %>% 
-  st_drop_geometry()
-
-# Grid-level attack panel
-grid_level_panel <- data %>% 
-  mutate(days_since_attack = as.numeric(days_since_attack)) %>% 
-  replace_na(replace = list(distance_km = 0,
-                            hours = 0,
-                            n_trips = 0,
-                            n_vessels = 0,
-                            n_ais_messages = 0)) %>% 
-  mutate(hours_div_vessel = hours / n_vessels,
-         dist_div_vessel = distance_km / n_vessels,
-         year = year(date),
-         month = month(date),
-         ym = paste(year, month, sep = "-")) %>% 
-  replace_na(replace = list(hours_div_vessel = 0,
-                            dist_div_vessel = 0))
-
 # Define some windows
-lag_window <- 7
 bef_window <- 7
 aft_window <- 7
 
 ## PROCESSING ------------------------------------------------------------------
-attacks <- grid_level_panel %>% 
+# Identify all dates with attacks
+attacks <- data %>% 
   filter(days_since_attack == 0) %>% 
   group_by(grid_id) %>% 
   arrange(date) %>%
-  mutate(data_days = date - min(date)) %>% 
-  filter(data_days >= bef_window + lag_window) %>%  # Remove observations that don't have enough pre-attack observations to calculate the moving mean
+  mutate(data_days = date - min(date)) %>% # Build a temporary variable that tells me how many days have passed between this observation and the first one, for each cell
+  filter(data_days >= bef_window) %>%  # Remove attacks for which we don't have enough enough observations leading to it
   select(grid_id, attack_date = date) %>% 
   mutate(attack_id = paste(grid_id, attack_date))
 
-event_study_panel <- grid_level_panel %>% 
-  left_join(attacks,
+# Now build the panel
+event_study_panel <- attacks %>% 
+  # First, we perform a many-to-many match
+  left_join(data,
             by = "grid_id",
             relationship = "many-to-many") %>% 
-  left_join(grid_fao, by = "grid_id") %>% 
-  filter(between(date, attack_date - (bef_window + lag_window), attack_date + aft_window)) %>% 
-  mutate(zone_year = paste(zone, year, sep = "_")) %>% 
+  # Then, for each attack date, we keep data that are within the before and after window defined above
+  filter(between(date,
+                 left = (attack_date - bef_window),
+                 right = (attack_date + aft_window))) %>%
+  # Extract year and month from the date
+  mutate(year = year(date),
+         month = month(date)) %>% 
   group_by(grid_id) %>% 
   arrange(date) %>% 
-  mutate(attacks = cumsum(days_since_attack == 0)) %>% 
+  mutate(attacks = cumsum(days_since_attack == 0)) %>%  # For each grid and date, calculate the number of historical attacks up to that point in time
   ungroup() %>% 
   arrange(grid_id, attack_date, date) %>% 
   group_by(grid_id, attack_date) %>% 
-  mutate(hours_w = rollapply(data = hours,
-                           width = lag_window,
-                           FUN = mean,
-                           fill = NA,
-                           align = "right"),
-         distance_w = rollapply(data = distance_km,
-                                width = lag_window,
-                                FUN = mean,
-                                fill = NA,
-                                align = "right"),
-         event = as.numeric(date - attack_date),
+  # Build the event-time variable, with date of attack = 0
+  mutate(event = as.numeric(date - attack_date),
          pre = 1 * (event < 0)) %>%
   ungroup() %>% 
-  filter(between(date, attack_date - bef_window, attack_date + aft_window)) %>% 
-  select(grid_id, attack_cluster, attack_id, lat_bin, lon_bin, attack_date, date, ym, zone_year, event, days_since_attack, attacks, contains("dist"), contains("hours"), n_trips)
+  select(grid_id, attack_cluster, attack_id,
+         lat_bin, lon_bin, attack_date,
+         date, year, month, zone, event, days_since_attack, attacks, contains("dist"), contains("hours"), n_trips)
 
-es_mod <- feols(c(hours, distance_km, n_trips) ~ attacks + days_since_attack + i(event, ref = "-1") | 
+## ESTIMATION ##################################################################
+# Fit the model
+es_mod <- feols(c(distance_km, time_hours, n_trips) ~ attacks + i(event, ref = "-1") | 
                   # Fixed effects
-                  grid_id + zone_year,
+                  grid_id + year ^ month ^ zone,
                 # SE specifictions
-                vcov = function(x)vcov_conley_hac(x,
-                                                  id = ~attack_id,
-                                                  time = ~date,
-                                                  lat = ~lat_bin,
-                                                  lon = ~lon_bin,
-                                                  cutoff = 75,
-                                                  lag = 14),
+                vcov = vcov_conley(lat = "lat_bin",
+                                   lon = "lon_bin",
+                                   cutoff = 100),
                 panel.id = ~attack_id + date,
                 fsplit= ~attack_cluster,
                 split.drop = "None",
                 lean = TRUE,
                 data = event_study_panel)
 
-
-
-
+# Extract model coefficients and SEs
 plot_data <- es_mod  %>% 
   map_dfr(broom::tidy, .id = "sample", conf.int = TRUE, conf.level = 0.95) %>% 
   filter(str_detect(term, "event")) %>% 
@@ -129,12 +95,11 @@ plot_data <- es_mod  %>%
          sample = str_remove_all(sample, "sample|[:punct:]| "),
          event = as.numeric(str_remove(term, "event::")))
 
+# Build the event-study plot
 plot <- ggplot(data = plot_data,
        aes(x = event, y = estimate, shape = var)) + 
   geom_vline(xintercept = 0, linetype = "dotted") +
   geom_hline(yintercept = 0, linetype = "dotted") +
-  # geom_errorbar(aes(ymin = conf.low,
-                    # ymax = conf.high)) +
   geom_errorbar(aes(ymin = estimate - std.error,
                     ymax = estimate + std.error),
                 color = "black",
@@ -162,7 +127,7 @@ ggsave(plot = plot,
        filename = here("figs", "grid_level_event_study.pdf"),
        width = 9,
        height = 11,
-       units = "cm")  
+       units = "cm")
 
 
 
