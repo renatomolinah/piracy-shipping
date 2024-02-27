3a739908536611d66221bdb305cd670418b58546
gmcdonald-sfg <gmcdonald@bren.ucsb.edu>
2024-02-27 14:21:41 -0800

    don't need emissions in this

--- sql/aggregate_spatial_shipping_activity.sql
+++ sql/aggregate_spatial_shipping_activity.sql
@@ -2,8 +2,7 @@ SELECT
   lat_bin,
   lon_bin,
   SUM(hours) hours,
-  SUM(distance_km) distance_km,
-  3.17 * SUM(main_fuel_consumption_mt_inst + aux_fuel_consumption_mt_inst) emissions_co2_mt
+  SUM(distance_km) distance_km
 FROM
   `emlab-gcp.piracy.gridded_data_0_5`
 GROUP BY
