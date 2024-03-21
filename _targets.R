3ef75a9ba713ea12ab3bc0d0ad343767d691d5c4
gmcdonald-sfg <gmcdonald@bren.ucsb.edu>
2024-03-21 08:11:31 -0700

    update pointer to nextcloud storage

--- _targets.R
+++ _targets.R
@@ -12,7 +12,7 @@ tar_source("r/functions.R")
 # emLab's Google Shared Drive where targets interm rds objects will live
 # This path will need to be modified by each user, since everyone has a different path to this directory
 # Uncomment this line, and run it for your personal machine
-# targets::tar_config_set(store = "/Users/gmcdonald/Library/CloudStorage/GoogleDrive-gmcdonald@ucsb.edu/Shared\ drives/emlab/Projects/current-projects/piracy/data/_targets")
+# targets::tar_config_set(store = "/Users/Shared/nextcloud/emLab/projects/current-projects/piracy/data/_targets"")
 
 # The data directory for raw data is set in relation to the targets directory
 data_directory <- targets::tar_config_get("store") |>
