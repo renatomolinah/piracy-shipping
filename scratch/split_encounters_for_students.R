aec0036915ed1b56f562b33fcb19f4a544c628ab
Juan Carlos Villaseñor-Derbez <jc_villasenor@miami.edu>
2025-03-08 12:51:47 -0500

    Split obs for students

--- scratch/split_encounters_for_students.R
+++ scratch/split_encounters_for_students.R
@@ -15,65 +15,65 @@
 # Load packages ----------------------------------------------------------------
 pacman::p_load(
   here,
+  xlsx,
   tidyverse,
   sf
 )
 
 # Load data --------------------------------------------------------------------
-all_encounters <- st_read("/Users/jcvd/Library/CloudStorage/Box-Box/piracy-shipping/Analysis-New/ASAM_N_shp/") |>
-  st_drop_geometry() |>
-  filter(between(year(dateofocc), 2012, 2021)) |> 
-  arrange(dateofocc)
+# all_encounters <- st_read("/Users/jcvd/Library/CloudStorage/Box-Box/piracy-shipping/Analysis-New/ASAM_N_shp/") |>
+#   st_drop_geometry() |>
+#   filter(between(year(dateofocc), 2012, 2021)) |>
+#   arrange(dateofocc)
+#
+# optB <- st_read("data/Asam_data_download_ASAM_shp_20250305") |>
+#   st_drop_geometry() |>
+#   filter(between(year(dateofocc), 2012, 2021)) |>
+#   arrange(dateofocc)
 
-optB <- st_read("data/Asam_data_download_ASAM_shp_20250305") |> 
-  st_drop_geometry() |>
-  filter(between(year(dateofocc), 2012, 2021)) |> 
-  arrange(dateofocc)
+asam_data <- st_read("data/Asam_data_download.gdb") |>
+  st_drop_geometry()
 
-optC <- st_read("data/Asam_data_download.gdb") |> 
-  st_drop_geometry() |>
-  filter(between(year(dateofocc), 2012, 2021)) |> 
-  arrange(dateofocc)
-
-all_encounters |> filter(reference == "2015-174") |> pull(descriptio)
-optB |> filter(reference == "2015-174") |> pull(descriptio)
-optC |> filter(reference == "2015-174") |> pull(description)
+# all_encounters |> filter(reference == "2015-174") |> pull(descriptio)
+# optB |> filter(reference == "2015-174") |> pull(descriptio)
+# optC |> filter(reference == "2015-174") |> pull(description)
 
 interns <- c(
-  "Grayson",
   "Sebastian",
   "Felipe",
   "Alejandro",
-  "Ariella"
+  "Ariella",
+  "Jnesse"
 )
 
 ## PROCESSING ##################################################################
 
 # X ----------------------------------------------------------------------------
 set.seed(5)
-subset <- all_encounters |>
-  filter(between(year(dateofocc), 2012, 2021)) |>
+subset <- asam_data |>
+  mutate(dateofocc = with_tz(dateofocc, tzone = "UTC")) |> # Need to do this because the geodatabase is shifting the times to UTC and Jan 1 was appearing as Dec 31
+  filter(between(year(dateofocc), 2012, 2023)) |>
+  select(reference, description) |>
   distinct() |>
   mutate(id = 1:n(),
          intern_A = sample(interns, size = length(reference), replace = T)) |>
   rowwise() |>
-  mutate(intern_B = sample(interns[!interns == intern_A], size = length(reference), replace = T))
+  mutate(intern_B = sample(interns[!interns == intern_A], size = length(reference), replace = T)) |>
+  select(id, reference, description, intern_A, intern_B)
 
 subs <- function(subset, name) {
   subset |>
   filter(intern_A == name | intern_B == name) |>
-  select(id, reference, description = descriptio) |>
-  mutate(keep = "",
-         attacker = "",
-         attacked = "",
-         notes = "")
+  mutate(keep = NA,
+         notes = NA) |>
+    select(id, reference, description, keep, notes)
 }
 
-Grayson <- subs(subset, "Grayson")
 Sebastian <- subs(subset, "Sebastian")
 Felipe <- subs(subset, "Felipe")
 Alejandro <- subs(subset, "Alejandro")
 Ariella <- subs(subset, "Ariella")
+Jnesse <- subs(subset, "Jnesse")
 
 ## TESTS #######################################################################
 
@@ -84,19 +84,17 @@ length(subset$reference[subset$intern_A == subset$intern_B])
 check_repeated <- function(x) {
   dim(x)[1] == length(unique(x$reference))
 }
-check_repeated(Grayson)
 check_repeated(Sebastian)
 check_repeated(Felipe)
 check_repeated(Alejandro)
 check_repeated(Ariella)
+check_repeated(Jnesse)
 
 # The number of concatenated unique references should be the same as the number of rows in the original data
-dim(subset)[1] == length(unique(c(Grayson$reference, Sebastian$reference, Felipe$reference, Alejandro$reference, Ariella$reference)))
-
-
+dim(subset)[1] == length(unique(c(Sebastian$reference, Felipe$reference, Alejandro$reference, Ariella$reference, Jnesse$reference)))
 
 # Lets combine (duplicate) all the data. Each reference should appear only twice
-combined <- bind_rows(Grayson, Sebastian, Felipe, Alejandro, Ariella)
+combined <- bind_rows(Sebastian, Felipe, Alejandro, Ariella, Jnesse)
 count(combined, reference) |>
   filter(!n == 2)
 count(combined, id) |>
@@ -112,15 +110,20 @@ left_join(A, B, by = join_by("intern_A" == "intern_B"), suffix = c("_A", "_B")) 
 
 ## EXPORT ######################################################################
 # X ----------------------------------------------------------------------------
-write_csv(x = subset,
-          file = here("data", "intern_data", "master.csv"))
-write_csv(x = Grayson,
-          file = here("data", "intern_data", "Grayson.csv"))
-write_csv(x = Sebastian,
-          file = here("data", "intern_data", "Sebastian.csv"))
-write_csv(x = Felipe,
-          file = here("data", "intern_data", "Felipe.csv"))
-write_csv(x = Alejandro,
-          file = here("data", "intern_data", "Alejandro.csv"))
-write_csv(x = Ariella,
-          file = here("data", "intern_data", "Ariella.csv"))
+write.xlsx(x = subset,
+          file = here("data", "intern_data", "master.xlsx"))
+write.xlsx(x = Sebastian,
+          file = here("data", "intern_data", "Sebastian.xlsx"),
+          showNA = F)
+write.xlsx(x = Felipe,
+          file = here("data", "intern_data", "Felipe.xlsx"),
+          showNA = F)
+write.xlsx(x = Alejandro,
+          file = here("data", "intern_data", "Alejandro.xlsx"),
+          showNA = F)
+write.xlsx(x = Ariella,
+          file = here("data", "intern_data", "Ariella.xlsx"),
+          showNA = F)
+write.xlsx(x = Jnesse,
+          file = here("data", "intern_data", "Jnesse.xlsx"),
+          showNA = F)
