972b84e3acc04b2ed6a1172dcef50d3d9328f773
jcvdav <juancarlos.villader@gmail.com>
2025-08-11 12:47:25 -0400

    shell script to upload cost data to bigquery

--- code/4.4_upload_full_pred_global_data.sh
+++ code/4.4_upload_full_pred_global_data.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 # Set path
-export PROJECT_PATH="/Users/juancarlosvillasenorderbez/GitHub/piracy-shipping/"
+export PROJECT_PATH="/Users/jcvd/GitHub/piracy-shipping/"
 
 # Upload all local csv files to GCS Bucket
-gsutil cp "$PROJECT_PATH"processed_data/full_pred_global.csv gs://piracy-emlab-gcp
+gsutil cp "$PROJECT_PATH"data/processed/full_pred_global.csv gs://piracy-emlab-gcp
 
 # Create a table in Big Query, specify the scheme, add a desciption, and name it
 bq mk --table \
---schema trip_id:STRING,attacks_3mo_num:INTEGER,wind_speed:FLOAT,wind_vector:FLOAT,country:STRING,vtype:STRING,size:INTEGER,hotspot:STRING,top_route:INTEGER,month:INTEGER,year:INTEGER,p_fuel:FLOAT,np_fuel:FLOAT,p_labor:FLOAT,np_labor:FLOAT,p_total:FLOAT,np_total:FLOAT,p_co2:FLOAT,np_co2:FLOAT,p_nox:FLOAT,np_nox:FLOAT,p_sox:FLOAT,np_sox:FLOAT \
+--schema trip_id:STRING,attacks_3mo_num:INTEGER,wind_speed:FLOAT,wind_vector:FLOAT,wave_height:FLOAT,country_pair:STRING,vessel_type:STRING,tonnage_decile:INTEGER,hotspot:STRING,top_route:INTEGER,month:INTEGER,year:INTEGER,p_fuel:FLOAT,np_fuel:FLOAT,p_labor:FLOAT,np_labor:FLOAT,p_total:FLOAT,np_total:FLOAT,p_co2:FLOAT,np_co2:FLOAT,p_nox:FLOAT,np_nox:FLOAT,p_sox:FLOAT,np_sox:FLOAT \
 --description "Model prediction of costs with and without pirate encounters" \
-emlab-gcp:piracy.full_pred_global_v_20240404
+emlab-gcp:piracy.full_pred_global_v_20250810
 
 # If I need to delete it, this is the command:
-#bq rm -f -t emlab-gcp:piracy.full_pred_global_v_20240404
+#bq rm -f -t emlab-gcp:piracy.full_pred_global_v_20250810
 
 # Upload from GCS bcket to Big Query table
 # We specify the format of the original data, tell it to ignore the first row (headers),
@@ -22,5 +22,5 @@ bq load \
 --skip_leading_rows=1 \
 --null_marker "NA" \
 --replace \
-emlab-gcp:piracy.full_pred_global_v_20240404 \
+emlab-gcp:piracy.full_pred_global_v_20250810 \
 gs://piracy-emlab-gcp/full_pred_global.csv
