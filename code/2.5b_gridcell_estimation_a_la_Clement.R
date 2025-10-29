2cdf68e09d1ac35bee9a6f363d42a920643ca2e9
jcvdav <juancarlos.villader@gmail.com>
2025-10-29 12:10:07 -0400

    Updated event study using Clement's method

--- code/2.5b_gridcell_estimation_a_la_Clement.R
+++ code/2.5b_gridcell_estimation_a_la_Clement.R
@@ -18,7 +18,7 @@ library(DIDmultiplegtDYN)
 library(tidyverse)
 library(here)
 
-theme_set(theme_minimal(base_size = 7))
+theme_set(theme_minimal(base_size = 10))
 
 # Load data --------------------------------------------------------------------
 panel <- readRDS(here("data", "processed", "attacks_and_activity_by_grid.rds")) |>
@@ -40,11 +40,10 @@ time <- "date"
 treatment <- "number_previous_attacks_grid_1_month"
 
 wrap <- function(outcome, force = F) {
-
   out_file <- here("data", "output", "es_a_la_Clement", paste0("es_mod_", outcome, ".rds"))
 
   if(!(file.exists(out_file)) || force) {
-    print(paste("File", out_file, "not found, proceeding to estimate model"))
+    print(paste("File", out_file, "not found or force was set to T, proceeding to estimate model"))
     reg <- did_multiplegt_dyn(df = panel,
                               outcome = outcome,
                               effects = effects,
@@ -53,7 +52,7 @@ wrap <- function(outcome, force = F) {
                               time = time,
                               treatment = treatment)
 
-    saveRDS(reg, file = out_file)
+    write_rds(reg, file = out_file)
   } else {
     print(paste("File", out_file, "already exist, skipping estimation"))
   }
@@ -61,10 +60,12 @@ wrap <- function(outcome, force = F) {
 
 outcomes <- c("time_hours",
               "distance_km",
+              "n_vessels",
+              "n_trips",
               "time_per_vessel",
               "dist_per_vessel")
 
-walk(outcomes, wrap, force = T)
+walk(outcomes, wrap, force = F)
 beepr::beep(4)
 
 
@@ -85,10 +86,12 @@ build_coef_table <- function(model) {
                      lb_ci = 0,
                      ub_ci = 0)) |>
     mutate(outcome = model$args$outcome,
-           title = case_when(outcome == "time_hours" ~ "Time",
-                             outcome == "time_per_vessel" ~ "Time / Vessel",
-                             outcome == "distance_km" ~ "Distance",
-                             outcome == "dist_per_vessel" ~ "Distance / Vessel")) |>
+           title = case_when(outcome == "time_hours" ~ "A) Occupancy Time (hours)",
+                             outcome == "distance_km" ~ "B) Distance Traveled (km)",
+                             outcome == "n_vessels" ~ "C) Transit (# Vessels)",
+                             outcome == "n_trips" ~ "D) Transit (# Trips)",
+                             outcome == "time_per_vessel" ~ "E) Occupancy per Vessel (hours / vessel)",
+                             outcome == "dist_per_vessel" ~ "F) Distance Traveled per Vessel (km / vessel)")) |>
     select(outcome, title, term, event, estimate, se, lb_ci, ub_ci, n, switchers, n_w, switchers_w) |>
     arrange(event)
 }
@@ -97,28 +100,38 @@ build_coef_table <- function(model) {
 build_event_study_plot <- function(coef_table) {
   ggplot(data = coef_table,
          mapping = aes(x = event, y = estimate)) +
-    geom_hline(yintercept = 0) +
-    geom_vline(xintercept = 0, linetype = "dashed") +
-    geom_point(size = 2, color = "#000000") +
-    geom_errorbar(aes(ymin = lb_ci, ymax = ub_ci), width = 0.3, color = "#000000") +
+    geom_vline(xintercept = 0, linetype = "dashed", color = "gray40") +
+    geom_hline(yintercept = 0, linetype = "dotted", color = "black") +
+    geom_linerange(aes(ymin = lb_ci, ymax = ub_ci),
+                  linewidth = 0.5, color = "black") +
+    geom_linerange(aes(ymin = estimate - se, ymax = estimate + se),
+                  linewidth = 1.5, color = "cadetblue") +
+    geom_point(size = 3, color = "cadetblue") +
     facet_wrap(~title,
-               scales = "free_y") +
-    labs(x = "Relative time to last period before treatment changes (t = 0)",
+               scales = "free_y", ncol = 2) +
+    labs(x = "Days Relative to Attack",
          y = "Estimate ± (std.error & 95% CI)") +
     theme_minimal(base_size = 12) +
     scale_x_continuous(breaks = seq(-7, 7, 2))
 }
 
-files <- list.files(here("data", "output", "es_a_la_Clement"), pattern = "es_mod", full.names = T)
+files <- list.files(here("data", "output", "es_a_la_Clement"),
+                    pattern = "es_mod", full.names = T)
 
 mods <- map(files, readRDS)
 
 es_plot_clement <- mods |>
   map_dfr(build_coef_table) |>
+  mutate(title = fct_relevel(.f = title,
+                             "Occupancy Time (hours)",
+                             "Distance Traveled (km)",
+                             "Transit (# Vessels)",
+                             "Transit (# Trips)",
+                             "Time / Vessel",
+                             "Distance / Vessel")) |> 
   build_event_study_plot()
 
 ggsave(plot = es_plot_clement,
-       filename = here("results", "figures_and_tables", "grid_level_event_study_multiple_gt_dyn.pdf"),
+       filename = here("results", "figures_and_tables", "grid_level_event_study_multiple_gt_dyn.png"),
        width = 9,
-       height = 6,
-       units = "cm")
+       height = 9)
