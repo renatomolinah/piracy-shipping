5dbc3413cb8db78dc6c3a21926be800a067b3a8a
Juan Carlos Villaseñor-Derbez <jc_villasenor@miami.edu>
2025-11-01 07:23:23 -0400

    Updating grid cell estimation to build specification tables

--- code/2.3_gridcell_estimation.R
+++ code/2.3_gridcell_estimation.R
@@ -28,30 +28,40 @@ panel <- function(res) {
 # =============================================================================
 
 # A function to perform the estimation
-run_estimation <- function(outcome_var, hotspot = "Global", res) {
-# If hotspot is provided, we filter the data to only include the hotspot
-# If hotspot is not provided, we use the entire dataset
+run_estimation <- function(outcome_var,
+                           spec = "~ post | id + year^month + day_of_week",
+                           hotspot = "Global",
+                           res = "0_5") {
+  # If hotspot is provided, we filter the data to only include the hotspot
+  # If hotspot is not provided, we use the entire dataset
   if(hotspot != "Global") {
     data <- panel(res) |> filter(attack_cluster == hotspot)
   } else {
     data <- panel(res)
   }
 
-fml <- as.formula(paste(outcome_var, "~ post | id + year^month + day_of_week + asam_subregion"))
+  # Main specification formula
+  fml <- as.formula(paste(outcome_var, spec))
 
-# Run the estimation
-results <- conleyreg(formula = fml,
-                     unit = "id",
-                     time = "date",
-                     lat = "lat_bin",
-                     lon = "lon_bin",
-                     data = data,
-                     dist_cutoff = 50,
-                     lag_cutoff = Inf,
-                     gof = TRUE)
+  cat("Estimating for", outcome_var, "in", hotspot, "at a resolution of", res)
 
-return(results)
+
+  # Run the estimation
+  results <- conleyreg(formula = fml,
+                       unit = "id",
+                       time = "date",
+                       lat = "lat_bin",
+                       lon = "lon_bin",
+                       data = data,
+                       dist_cutoff = 50,
+                       lag_cutoff = Inf,
+                       gof = TRUE)
+
+  return(results)
 }
+
+
+# Define all outcomes, specifications, resolutions and samples -----------------
 
 # Outcomes of interest
 outcome_vars <- c("asinh(time_hours)",
@@ -61,25 +71,49 @@ outcome_vars <- c("asinh(time_hours)",
                   "asinh(time_vessels)",
                   "asinh(dist_vessels)")
 
-# Sub-sample specifications
-hotspots <- c("Global", sort(unique(panel("0_5")$attack_cluster)))
+# All specifications
+all_specs <- c("~ post",
+               "~ post | id ",
+               "~ post | id + year^month")
 
-# Run post-regressions for different outcome variables using conleyreg with asinh transformation
+# Sub-sample specifications
+hotspots <- c("Global", "G. of Aden", "G. of Guinea", "S.E. Asia")
+
+# Different resolutions
+res <- c("0_1", "0_5", "1")
+
+
+# Run main post-regressions ----------------------------------------------------
 models <- expand_grid(outcome_var = outcome_vars,
                       hotspot = hotspots,
-                      res = c("0_1", "0_5", "1")) |>
-  filter(!hotspot == "None") |>
-  mutate(model = pmap(.l = list(outcome_var, hotspot, res), run_estimation),
+                      spec = "~ post | id + year^month + day_of_week",
+                      res = res) |>
+  mutate(model = pmap(.l = list(outcome_var = outcome_var, spec = spec, hotspot = hotspot, res = res), run_estimation),
          coefficients = map(model, coefficients),
          n = map_dbl(model, nobs))
 
 names(models$coefficients) <- models$hotspot
 
+# Run specification tables post-regressions ------------------------------------
+spec_tables <- expand_grid(outcome_var = outcome_vars,
+                           spec = all_specs) |>
+  mutate(model = pmap(.l = list(outcome_var = outcome_var, spec = spec),
+                      run_estimation,
+                      hotspot = "Global",
+                      res = "0_5"),
+         coefficients = map(model, coefficients),
+         n = map_dbl(model, nobs))
+
+
 # Robustness on AIS disabling events
 AIS_disab_models <- expand_grid(outcome_var = "asinh(n_ais_disabling)",
+                                spec = "~ post | id + year^month + day_of_week",
                                 hotspot = c("Global", "G. of Aden", "G. of Guinea"),
-                                res = "1") |>
-  mutate(model = pmap(.l = list(outcome_var, hotspot, res), run_estimation),
+                                res = "0_5") |>
+  mutate(model = pmap(.l = list(outcome_var = outcome_var,
+                                spec = spec,
+                                hotspot = hotspot,
+                                res = res), run_estimation),
          coefficients = map(model, coefficients),
          n = map_dbl(model, nobs))
 
@@ -90,3 +124,7 @@ names(AIS_disab_models$coefficients) <- AIS_disab_models$hotspot
 # # =============================================================================
 save(models,
      file = here("data", "output", "gridcell_models.RData"))
+save(spec_tables,
+     file = here("data", "output", "gridcell_spec_models.RData"))
+save(AIS_disab_models,
+     file = here("data", "output", "gridcell_AIS_disabling.RData"))
