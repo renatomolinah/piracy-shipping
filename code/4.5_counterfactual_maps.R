06592f1ab842625aed27800b69738ead54a82c6d
Renato E. Molina <renato.molina.h@gmail.com>
2026-05-01 21:11:54 -0400

    remove dead commented-out saveRDS in counterfactual maps

--- code/4.5_counterfactual_maps.R
+++ code/4.5_counterfactual_maps.R
@@ -119,9 +119,6 @@ total_grided <- local_grided %>%
          local = (nox * sc_nox / 1e6) + (sox * sc_sox / 1e6),
          total = private + public)
 
-# saveRDS(object = total_grided,
-#         file = here("output_data", "gridded_public_and_private_counterfactual_predictions.rds"))
-
 # --- Zonal summaries ---
 
 total_by_asam <- total_grided %>%
